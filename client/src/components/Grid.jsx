d52f4b94961f36d8f6d45f81945ce2843d0e0cb4
Senou Lynn <senou.lynn@gmail.com>
2021-05-18 15:30:49 -0400

    Added winning conditions

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -8,21 +8,90 @@ export default function Grid(props) {
 
 
     //Create a workable array with glossary of availbale numbers
-    let gridArray= []
-    for(let i = initialMin; i <= initialMax; i++ ){
+    //This will allow you to map over and create divs with keys and content mirroring index
+    let gridArray = []
+    for (let i = initialMin; i <= initialMax; i++) {
         gridArray.push(i)
     }
-    console.log(gridArray)
+
+    let newMin;
+    let newMax;
+    let newGuess;
+    function startSearch() {
+        console.log("You started your search")
+        
+        //newMin/newMax will be undefined on first go
+        if(!newMin && !newMax){
+            newMin = initialMin;
+            newMax = initialMax;
+
+            randomNum(newMin, newMax)
+        } else {
+            randomNum()
+        }
+
+
+    }
+
+    //Generates a random number within range
+    function randomNum(newMin, newMax) {
+        console.log("Getting Random Number")
+
+        newGuess = Math.floor(Math.random() * (newMax - newMin + 1) + newMin)
+        console.log( newGuess)
+
+        checkNum()
+
+        
+    }
+
+    //Check if number is the right one
+    //If guessNum is incorrect AND smaller than the target number then... res
+    //If guessNum is incorrect AND larger than the target number then...
+    function checkNum() {
+        console.log("Checknum newGuess: ", newGuess)
+        console.log("Checknum intialNum: ", initialNum)
+
+        if(newGuess !== initialNum && newGuess < initialNum){
+            console.log("random number is less than target number")
+            changeMin()
+           
+        } else if (newGuess !== initialNum && newGuess > initialNum){
+            console.log("random number is greater than target number")
+            changeMax()
+
+        } else if (newGuess === initialNum) {
+            console.log("Winning condition")
+        }
+
+    }
+
+    //If newGuess < initialNum
+        //Change minimum
+        //Add class to GuessNum box
+
+    function changeMin() {
+
+    }
+
+    //If newGuess > initialNum 
+        //Change maximum
+        //Add class to GuessNum box 
+    function changeMax() {
+
+    }
 
     return (
         <div id="grid">
             <h3>Grid Area</h3>
+            <button onClick={startSearch}>Start Search</button>
             <div className="grid-container">
-            {gridArray.map((num) => {
-                return(
-                    <div key={num} className={"gridbox"}>{num}</div>
-                )
-            })}
+
+                {gridArray.map((num) => {
+                    return (
+                        <div key={num} className={"gridbox"}>{num}</div>
+                    )
+                })}
             </div>
         </div>
     )
